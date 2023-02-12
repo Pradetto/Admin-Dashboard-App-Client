b8e4abc2df166156000604e7cc28b6c66d6a695a
Michael Pradetto <pradetto5@gmail.com>
2023-02-11 18:53:10 -0600

    navbar and sidebar

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { api } from "state/api";
 
 // we can do this because of jsconfig.json set base folder as SRC
 import globalReducer from "state";
@@ -11,8 +13,11 @@ import globalReducer from "state";
 const store = configureStore({
   reducer: {
     global: globalReducer,
+    [api.reducerPath]: api.reducer,
   },
+  middleware: (getDefault) => getDefault().concat(api.middleware),
 });
+setupListeners(store.dispatch);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
