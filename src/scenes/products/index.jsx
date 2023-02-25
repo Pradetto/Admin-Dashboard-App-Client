cdaf23d70a884fa18ee7803bd1a77fc3e8cecfb2
Michael Pradetto <pradetto5@gmail.com>
2023-02-24 21:47:03 -0600

    API

---
+++ src/scenes/products/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import {
+    Box,
+    Card,
+    CardActions,
+    CardContent,
+    Collapse,
+    Button,
+    Typography,
+    Rating,
+    useTheme,
+    useMediaQuery
+} from '@mui/material'
+import useGetProductQuery from ''
+
+const Product = () => {
+
+  return (
+    <div>Product</div>
+  )
+}
+
+export default Product
